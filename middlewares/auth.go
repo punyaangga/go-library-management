e0c734e88c31c18fb0a686c358e82361b383677e
punyaangga <anggafantiya@gmail.com>
2024-11-23 10:33:37 +0700

    change message authorization

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -14,7 +14,7 @@ func JWTAuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		authHeader := c.GetHeader("Authorization")
 		if authHeader == "" {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header required"})
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is required"})
 			c.Abort()
 			return
 		}
